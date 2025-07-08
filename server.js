d704150c9a492218b0f5b76cc0cf50b8675a167b
Sujal Sanjay Ghorpade <sujalghorpade1516@gmail.com>
2025-07-09 00:35:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://waterpurifier-six.vercel.app', 
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/admin", require("./routes/adminRoutes"));
